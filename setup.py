96640569b6a402dd57a9e88a8faf1f0f2a1ecda1
Daniel Trnka <daniel.trnka@gmail.com>
2018-02-19 14:26:55 +0100

    New measurements

--- setup.py
+++ setup.py
@@ -3,13 +3,11 @@ from widgets import Touch
 
 def setup_charts(app):
     app.next_row()
-    #plot(['RX', 'RY'], [0, 65535], colspan=2)
-    #plot(['USX', 'USY'], [0, 0.1], colspan=2)
     app.plot(['nx', 'ny'], [-1, 1], colspan=2, title='normal')
-    app.plot(['vx', 'vy'], [-1000, 1000], colspan=2, title='speed')
+    app.plot(['vx', 'vy'], [-1, 1], colspan=2, title='speed')
 
     app.next_row()
-    app.plot(['nsx', 'nsy'], [-1, 1], colspan=2, title='normalized speed')
+    app.plot(['cx', 'cy'], [-1, 1], colspan=2, title='change')
 
     p3 = app.win.addPlot(colspan=1, title="Touch resistance")
     p3.setXRange(0, 65535)

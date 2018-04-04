ac81f0a1f33a685d7fa0df8bb850dda1491fc58a
Daniel Trnka <daniel.trnka@gmail.com>
2018-04-04 20:12:29 +0200

    support for new uart protocol

--- input.py
+++ input.py
@@ -1,143 +1,23 @@
 import logging
-import sys
 import threading
-import time
-import itertools
-import struct
 import pandas as pd
-from collections import deque
 from queue import Queue
 
-from serial import Serial, SerialException
-
-
-def parse_line(line):
-    def fix(pair):
-        return pair[0], float(pair[1])
-
-    row = dict([fix(i.split('=', 2)) for i in line.split(' ') if len(i.split('=', 2)) == 2])
-    return row
-
-
-class TextLineReader:
-    """RX=val RY=val\n"""
-
-    def read_next(self, io):
-        line = io.readline()
-        row = parse_line(line)
-
-
-class BinaryReader:
-    def __init__(self):
-        fields = [
-            'c',
-            'v',
-            'pos',
-            'rv',
-            'ra',
-            'n',
-            'r',
-            'us',
-            'raw'
-        ]
-
-        self.measures = list(itertools.chain(*[[f + 'x', f + 'y'] for f in fields]))
-
-    def read_next(self, io):
-        prev = 0
-        while True:
-            cur = io.read()[0]
-            if prev == 0xab and cur == 0xcd:
-                break
-            prev = cur
-
-        x = io.read(len(self.measures) * 4)
-        values = struct.unpack("{}f".format(len(self.measures)), x)
-        return dict(zip(self.measures, values))
-
-
-class RunningAverage:
-    def __init__(self, keep_last=10):
-        self.last = deque()
-        self.size = keep_last
-
-    def add(self, val):
-        self.last.append(val)
-
-        if len(self.last) > self.size:
-            self.last.popleft()
-
-    def median(self):
-        items = sorted(self.last)
-        return self.last[int(len(items) / 2)]
+import uart
+from serial import Serial
 
 
 class SerialSource:
-    def __init__(self, device, baud, reader):
-        self.serial = Serial(device, baudrate=baud)
-        self.measures_keys_avg = RunningAverage()
-        self.reader = reader
-
-    def handle_lines(self, announce):
-        self._flush_input()
-        while True:
-            try:
-                row = self.reader.read_next(self.serial)
-
-                self.measures_keys_avg.add(len(row.keys()))
-                if self.measures_keys_avg.median() != len(row.keys()):
-                    logging.debug("Dropped measurement", row)
-                    continue
-
-                announce(row)
-            except Exception as e:
-                logging.exception(e)
-                self._reopen()
-
-    def write(self, text):
-        self.serial.write(text.encode())
-
-    def _reopen(self):
-        self.serial.close()
-        time.sleep(0.5)
-
-        while True:
-            try:
-                self.serial.open()
-                return
-            except BaseException as e:
-                logging.exception(e)
-
-            time.sleep(0.5)
-
-    def _readline(self):
-        return self.serial.readline().decode().strip()
-
-    def _flush_input(self):
-        self.serial.timeout = 0.005
-        while self.serial.readline():
-            pass
-
-        for i in range(0, 50):
-            self.serial.readline()
-
-        self.serial.timeout = None
-
-
-class StreamSource:
-    def __init__(self, source, reader):
-        self.source = source
-        self.reader = reader
+    def __init__(self, device, baud):
+        self.source = uart.Client(Serial(device, baudrate=baud))
 
     def handle_lines(self, announce):
         while True:
             try:
-                announce(self.reader.read_next(self.source))
+                row = self.source.read_next()
+                #announce(row[1])
             except Exception as e:
                 logging.exception(e)
-
-    def write(self, msg):
-        raise NotImplementedError()
 
 
 class Input:
@@ -148,9 +28,7 @@ class Input:
             with self.lock:
                 self.measured.append(row)
 
-        reader = BinaryReader()
-        self.serial = SerialSource(args.serial, args.baudrate, reader) if args.serial else StreamSource(
-            sys.stdin.buffer, reader)
+        self.serial = SerialSource(args.serial, args.baudrate)
         self.pause = False
         self.measurements = Queue()
         self.thread = threading.Thread(target=self.serial.handle_lines, args=[new_measurement])
@@ -170,12 +48,13 @@ class Input:
 
         text = " ".join([cmd] + list(map(mapper, arguments)))
         print(text)
-        self.serial.write(text + "\n")
+        #self.serial.write(text + "\n")
 
     def start(self):
         self.thread.start()
 
     def get_available(self, max_measures=10):
+        print((self.measurements.qsize()))
         measurements = []
         while not self.measurements.empty() and max_measures > 0:
             measurements.append(self.measurements.get())
---
+++ uart.py
@@ -0,0 +1,119 @@
+import itertools
+import logging
+
+from cobs import cobs
+from serial import Serial, SerialException
+import struct
+import time
+
+
+class Command:
+    def __init__(self, num, *args):
+        self.id = num
+        self.args = args
+
+    def __call__(self, **kwargs):
+        args = [kwargs[i[0]] for i in self.args]
+        fmt = "".join([i[1] for i in self.args])
+
+        binary = struct.pack(("=B" + fmt), self.id, *args)
+        encoded = cobs.encode(bytearray(binary))
+        s.write(encoded + b"\0")
+        print(len(encoded))
+
+
+class MeasureEncoder:
+    def __init__(self):
+        self.fields = [
+            'c',
+            'v',
+            'pos',
+            'rv',
+            'ra',
+            'n',
+            'r',
+            'us',
+            'raw'
+        ]
+
+    def __call__(self, body, *args, **kwargs):
+        measures = list(itertools.chain(*[[f + 'x', f + 'y'] for f in self.fields]))
+        values = struct.unpack("{}f".format(len(measures)), body)
+        return dict(zip(measures, values))
+
+
+class Client:
+    def __init__(self, serial):
+        self.serial = serial
+        self.cmds = {
+            'reset': Command(0),
+            'pos': Command(1, ('x', 'I'), ('y', 'I')),
+            'pid': Command(2, ('p', 'd'), ('i', 'd'), ('d', 'd'))
+        }
+        self.encoders = {
+            128: MeasureEncoder()
+        }
+
+    def _reopen(self):
+        self.serial.close()
+        time.sleep(0.5)
+
+        while True:
+            try:
+                self.serial.open()
+                return
+            except BaseException as e:
+                logging.exception(e)
+
+            time.sleep(0.5)
+
+    def read_next(self):
+        while True:
+            try:
+                data = self.serial.read_until(b"\0")[:-1]
+
+                data = cobs.decode(data)
+
+                cmd = data[0]
+                body = data[1:]
+
+                return cmd, self.encoders[cmd](body)
+            except SerialException as e:
+                logging.exception(e)
+                self._reopen()
+            except Exception as e:
+                logging.exception(e)
+
+    def __getattr__(self, item):
+        return self.cmds[item]
+
+
+if __name__ == "__main__":
+    s = Serial("/dev/ttyUSB0", baudrate=460800)
+    c = Client(s)
+
+
+    def reading():
+        print("started")
+
+        # drop garbage
+        s.read_until(b"\0")
+
+        while True:
+            try:
+                print(c.read_next())
+            except Exception as e:
+                logging.exception(e)
+
+
+    import threading
+
+    threading.Thread(target=reading).start()
+
+    import time
+
+    while True:
+        c.pos(x=int(170 / 2), y=80)
+        time.sleep(1)
+        c.pos(x=int(170 / 2), y=160)
+        time.sleep(1)
